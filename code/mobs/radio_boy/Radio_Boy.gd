200662b81eee327d43d9287e12807a7b4f52670d
OverDriveZ <64517916+OverDriveZ@users.noreply.github.com>
2024-02-17 16:40:01 +0100

    absolute pain

---
+++ code/mobs/radio_boy/Radio_Boy.gd
@@ -0,0 +1,71 @@
+extends CharacterBody3D
+
+const SPEED = 8.0
+const JUMP_VELOCITY = 4.5
+
+#Camera Vars
+var camera_offset_z = 12
+
+
+#Mob Vars
+var last_direction
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 0 # ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("move_up", "move_down", "move_right", "move_left")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:  #Handle directions (which are only two...)
+		if direction.z < 0:  #We are moving right
+			last_direction = "right"
+			$Radio_Boy_Model.rotation.y = 0
+
+		if direction.z > 0:  #We are moving left
+			last_direction = "left"
+			$Radio_Boy_Model.rotation.y = 3.14  #I guess we are not using regular degrees, we are using radians
+
+		velocity.z = direction.z * SPEED
+		$Radio_Boy_Model/AnimationPlayer.play("Walk test")
+	else:
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		$Radio_Boy_Model/AnimationPlayer.play("Wink")
+		if last_direction == "right":
+			$Radio_Boy_Model.rotation.y = -0.611
+		elif last_direction == "left":
+			$Radio_Boy_Model.rotation.y = 3.751
+
+	move_and_slide()
+
+#func move_camera(direction, time_taken_to_move, offset_to_fill):
+	#var camera_steps = offset_to_fill * 0.001
+	#var camera_time = time_taken_to_move * 0.001
+	#if direction == "right":
+		#print(camera_steps)
+		#for i in range(1000):
+			#$Camera3D.position.z += camera_steps
+			#await get_tree().create_timer(time_taken_to_move).timeout
+	#
+#
+	#
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
