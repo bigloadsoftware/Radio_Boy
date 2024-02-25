25719814fe8dfdbde55f966bc140e6ba0afcd98f
OverDriveZ <64517916+OverDriveZ@users.noreply.github.com>
2024-02-25 15:01:57 +0100

    Camera Re-Center Fix

--- code/mobs/radio_boy/Camera_Pivot.gd
+++ code/mobs/radio_boy/Camera_Pivot.gd
@@ -7,14 +7,18 @@ var camera_recenter_speed = 10
 var camera_aim_reach = 12  #lower number means that character can aim further away
 
 #Outer Node Vars
-var rb_state_walk_or_run_previous
+var rb_movement_state_previous
 
 func _physics_process(_delta):
 	#-->Handle aim
 	#To do list:
 	#-This one needs a timer so it doesn't get processed every machine cycle (so we can make this a little bit more efficient)
 	if Input.is_action_just_pressed("aim"):
-		rb_state_walk_or_run_previous = $"..".rb_state_walk_or_run
+		rb_movement_state_previous = $"..".rb_movement_state
+		$"..".Handle_Movement("Walk", 1)
+
+	if Input.is_action_just_released("aim"):
+		$"..".Handle_Movement("Previous")
 
 	if Input.is_action_pressed("aim"):
 		mouse_vector = (Vector2(get_viewport().get_mouse_position().x - (get_viewport().size.x/2), get_viewport().get_mouse_position().y - (get_viewport().size.y/2)))
@@ -28,12 +32,6 @@ func _physics_process(_delta):
 	else:
 		velocity.z = (camera_offset.z - position.z) * camera_recenter_speed
 		velocity.y = (camera_offset.y - position.y) * camera_recenter_speed
-
-	if rb_state_walk_or_run_previous == "Run":
-		if Input.is_action_just_pressed("aim"):
-			$"..".Handle_Walk_Run_Toggle("Run")
-		elif Input.is_action_just_released("aim"):
-			$"..".Handle_Walk_Run_Toggle("Walk")
 
 	#<--
 
