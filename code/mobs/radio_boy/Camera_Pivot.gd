6bd63a21a4073503227e75a91c626a2d8fd05d5e
Ramparos <alessio.toccaceli98@gmail.com>
2024-03-03 17:36:42 +0100

    crouch_fix

--- code/mobs/radio_boy/Camera_Pivot.gd
+++ code/mobs/radio_boy/Camera_Pivot.gd
@@ -5,11 +5,10 @@ var mouse_vector
 var joypad_vector = Vector2(0, 0)
 var joypad_trigger_threshold : float = 0.05
 var camera_offset = transform.basis * Vector3(0, 7, 0) #our used vars are y and z
-var camera_aim_speed = 10 #what is this variable exactly?
 var camera_recenter_character_speed = 10
 var camera_center_cursor_speed = 12
 var camera_aim_reach = 20  #(i think that it is higher) #You're right.... -Leon
-var camera_aim_reach_joystick = 14
+var camera_aim_reach_joystick = 10
 
 #Outer Node Vars
 var rb_movement_state_previous
